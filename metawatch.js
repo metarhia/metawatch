cf7a835cf8f8eed377d8458b97fe0d7b5758e841
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-28 23:50:50 +0300

    Initial implementation
    PR-URL: https://github.com/metarhia/metawatch/pull/6

--- metawatch.js
+++ metawatch.js
@@ -1,5 +1,28 @@
 'use strict';
 
-console.log('Module stub');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {};
+const watch = (targetPath, listener) => {
+  fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
+    for (const file of files) {
+      if (file.isDirectory()) {
+        const dirPath = path.join(targetPath, file.name);
+        watch(dirPath, listener);
+      }
+    }
+    fs.watch(targetPath, (event, fileName) => {
+      const filePath = path.join(targetPath, fileName);
+      try {
+        fs.stat(filePath, (err, stats) => {
+          if (stats.isDirectory()) watch(filePath, listener);
+        });
+      } catch {
+        return;
+      }
+      listener(event, fileName);
+    });
+  });
+};
+
+module.exports = watch;
