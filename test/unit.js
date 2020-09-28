db32a13f9b4bf0151d4c6c893b5dcef34d4329d7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-28 23:53:25 +0300

    File change test
    PR-URL: https://github.com/metarhia/metawatch/pull/6

--- test/unit.js
+++ test/unit.js
@@ -1,9 +1,45 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 const metawatch = require('..');
 const metatests = require('metatests');
 
-metatests.test('Test stub', async test => {
-  test.strictSame(metawatch, {});
-  test.end();
+const WATCH_TIMEOUT = 200;
+const TEST_TIMEOUT = 2000;
+
+const dir = process.cwd();
+
+const targetPath = path.join(dir, 'test/example');
+
+metatests.test('Watch file change ', test => {
+  test.strictSame(typeof metawatch, 'function');
+
+  const timeout = setTimeout(() => {
+    test.fail();
+  }, TEST_TIMEOUT);
+
+  let count = 0;
+  const expected = process.platform === 'darwin' ? 1 : 2;
+
+  metawatch(targetPath, (event, fileName) => {
+    count++;
+    test.strictSame(event, 'change');
+    test.strictSame(fileName, 'file.ext');
+    clearTimeout(timeout);
+    if (count === expected) {
+      test.end();
+      setTimeout(() => {
+        process.exit(0);
+      }, WATCH_TIMEOUT);
+    }
+  });
+
+  setTimeout(() => {
+    const filePath = path.join(targetPath, 'file.ext');
+    fs.writeFile(filePath, 'example', 'utf8', err => {
+      test.error(err, 'Can not write file');
+    });
+  }, WATCH_TIMEOUT);
 });
