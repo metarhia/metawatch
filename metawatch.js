beba76032407b10090f41f46a834a90e61ec82fe
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-21 02:06:28 +0200

    Rewrite to class syntax
    PR-URL: https://github.com/metarhia/metawatch/pull/13

--- metawatch.js
+++ metawatch.js
@@ -2,27 +2,50 @@
 
 const fs = require('fs');
 const path = require('path');
+const { EventEmitter } = require('events');
 
-const watch = (targetPath, listener) => {
-  fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
-    for (const file of files) {
-      if (file.isDirectory()) {
-        const dirPath = path.join(targetPath, file.name);
-        watch(dirPath, listener);
-      }
-    }
-    fs.watch(targetPath, (event, fileName) => {
+class DirectoryWatcher extends EventEmitter {
+  constructor() {
+    super();
+    this.watchers = new Map();
+  }
+
+  watchDirectory(targetPath) {
+    if (this.watchers.get(targetPath)) return;
+    const watcher = fs.watch(targetPath, (event, fileName) => {
       const filePath = path.join(targetPath, fileName);
       try {
         fs.stat(filePath, (err, stats) => {
-          if (stats.isDirectory()) watch(filePath, listener);
+          if (stats.isDirectory()) this.watch(filePath);
         });
       } catch {
         return;
       }
-      listener(event, fileName);
+      this.emit(event, fileName);
     });
-  });
-};
+    this.watchers.set(targetPath, watcher);
+  }
 
-module.exports = watch;
+  watch(targetPath) {
+    const watcher = this.watchers[targetPath];
+    if (watcher) return;
+    fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
+      for (const file of files) {
+        if (file.isDirectory()) {
+          const dirPath = path.join(targetPath, file.name);
+          this.watch(dirPath);
+        }
+      }
+      this.watchDirectory(targetPath);
+    });
+  }
+
+  unwatch(path) {
+    const watcher = this.watchers[path];
+    if (!watcher) return;
+    watcher.close();
+    this.watchers.delete(path);
+  }
+}
+
+module.exports = { DirectoryWatcher };
