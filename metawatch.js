fd626b3f12a85da013a8a1e0c58d43bc634c4da0
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-21 03:41:44 +0200

    Implement timeout
    Closes: https://github.com/metarhia/metawatch/issues/5
    
    PR-URL: https://github.com/metarhia/metawatch/pull/16

--- metawatch.js
+++ metawatch.js
@@ -4,10 +4,38 @@ const fs = require('fs');
 const path = require('path');
 const { EventEmitter } = require('events');
 
+const WATCH_TIMEOUT = 5000;
+
 class DirectoryWatcher extends EventEmitter {
-  constructor() {
+  constructor(options = {}) {
     super();
     this.watchers = new Map();
+    this.timeout = options.timeout || WATCH_TIMEOUT;
+    this.timer = null;
+    this.queue = [];
+    this.index = [];
+  }
+
+  post(event, fileName) {
+    if (this.timer) clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.sendQueue();
+    }, this.timeout);
+    const key = event + ':' + fileName;
+    if (this.index.includes(key)) return;
+    this.queue.push({ event, fileName });
+    this.index.push(key);
+  }
+
+  sendQueue() {
+    if (!this.timer) return;
+    clearTimeout(this.timer);
+    this.timer = null;
+    for (const item of this.queue) {
+      this.emit(item.event, item.fileName);
+    }
+    this.queue = [];
+    this.index = [];
   }
 
   watchDirectory(targetPath) {
